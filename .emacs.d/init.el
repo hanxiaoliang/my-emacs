33838f413e7cd8cb9ec663217427783821f91f38
hanxiaoliang <510692737@qq.com>
2016-08-13 20:15:08 +0800

    new file
    new file

---
+++ .emacs.d/init.el
@@ -0,0 +1,97 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(when (>= emacs-major-version 24)
+    (require 'package)
+    (package-initialize)
+    (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t))
+
+;; cl - Common Lisp Extension
+(require 'cl)
+
+;; Add Packages
+(defvar my/packages '(
+	       ;; --- Auto-completion ---
+	       company
+	       ;; --- Better Editor ---
+	       hungry-delete
+	       swiper
+	       counsel
+	       smartparens
+	       ;; --- Major Mode ---
+	       js2-mode
+	       ;; --- Minor Mode ---
+	       nodejs-repl
+	       exec-path-from-shell
+	       ;; --- Themes ---
+	       monokai-theme
+	       ;; solarized-theme
+	       ) "Default packages")
+
+(setq package-selected-packages my/packages)
+
+(defun my/packages-installed-p ()
+    (loop for pkg in my/packages
+	  when (not (package-installed-p pkg)) do (return nil)
+	  finally (return t)))
+
+(unless (my/packages-installed-p)
+    (message "%s" "Refreshing package database...")
+    (package-refresh-contents)
+    (dolist (pkg my/packages)
+      (when (not (package-installed-p pkg))
+	(package-install pkg))))
+
+;; Find Executable Path on OS X
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+(tool-bar-mode -1)
+
+(scroll-bar-mode -1)
+
+(setq inhibit-splash-screen 1)
+
+(global-linum-mode 1)
+
+(defun open-my-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+(global-set-key (kbd "<f2>") 'open-my-init-file)
+
+(global-company-mode t)
+
+(setq-default cursor-type 'bar)
+
+(setq make-backup-files nil)
+
+(require 'org)
+(setq org-src-fontify-natively t)
+
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-item 10)
+(delete-selection-mode 1)
+
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+(global-hl-line-mode t)
+
+(load-theme 'monokai t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
